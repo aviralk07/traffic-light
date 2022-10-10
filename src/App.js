fc23b10ba16b8a72883a009885cdb295f73785b7
Aviral Kalra <aviralkalra@Avirals-MacBook-Air.local>
2022-10-11 04:52:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, usestate } from "react";
+import Light from "./Light";
 
 function App() {
+  const colors = ["red", "yellow", "green"];
+  const [lit, setlit] = useState("red");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {colors.map((color) => {
+        return <Light color={color} lit={lit} setLit={setlit} />;
+      })}
     </div>
   );
 }
